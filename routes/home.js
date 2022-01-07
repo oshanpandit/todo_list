f70cbcdb676bdd406ecb532d5d4c76bf50b9d47d
oshan pandit <oshanpandit@gmail.com>
2022-01-07 16:18:28 +0530

    add css file

--- routes/home.js
+++ routes/home.js
@@ -2,6 +2,8 @@ const express=require('express');
 
 const router=express.Router();
 
+router.use(express.static('assets'));
+
 const homepageController=require('../controllers/homePage_controller');
 
 router.get('/',homepageController.home);
