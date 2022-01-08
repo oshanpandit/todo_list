277bae1e467749e0b9240112bd8531d3b33dad91
oshan pandit <oshanpandit@gmail.com>
2022-01-08 19:17:04 +0530

    add basic style to the main page

---
+++ database.js
@@ -0,0 +1,20 @@
+var todoList=[
+
+    {
+
+        task:"Eat vegetables"
+
+    },
+
+    {
+
+        task:"Study for test"
+    },
+
+    {
+
+        task:"Sleep"
+    }
+];
+
+module.exports=todoList;
